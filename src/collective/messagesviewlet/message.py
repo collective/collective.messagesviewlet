17499d5541f21af7e337f616840dcb10e02a3c56
boulch <christophe.boulanger@imio.be>
2015-08-25 11:58:38 +0200

    Add new method that return vocabularies for title traductions.

--- src/collective/messagesviewlet/message.py
+++ src/collective/messagesviewlet/message.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from zope import schema
 from zope.interface import Interface
-
+from zope.i18n import translate
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from collective.messagesviewlet import _
 from plone.app.textfield import RichText
+
+
+def msg_type(context):
+    terms = []
+    terms.append(SimpleTerm("info", title=translate("Info", domain="collective.messagesviewlet",
+                            context=context.REQUEST)))
+    terms.append(SimpleTerm("warning", title=translate("Warning", domain="collective.messagesviewlet",
+                            context=context.REQUEST)))
+    terms.append(SimpleTerm("important", title=translate("Important", domain="collective.messagesviewlet",
+                            context=context.REQUEST)))
+    return SimpleVocabulary(terms)
 
 
 class IMessage(Interface):
@@ -31,7 +43,7 @@ class IMessage(Interface):
     msg_type = schema.Choice(
         title=_(u"Message type"),
         required=True,
-        values=[_(u'Info'), _(u'Warning'), _(u'Important')]
+        source=msg_type
     )
 
     location = schema.Choice(
