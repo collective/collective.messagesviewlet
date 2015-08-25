a5ba7a843167b9749acd78a18afe9817cce66a8a
boulch <christophe.boulanger@imio.be>
2015-08-25 11:28:41 +0200

    Add new message properties

--- src/collective/messagesviewlet/message.py
+++ src/collective/messagesviewlet/message.py
@@ -3,6 +3,7 @@ from zope import schema
 from zope.interface import Interface
 
 from collective.messagesviewlet import _
+from plone.app.textfield import RichText
 
 
 class IMessage(Interface):
@@ -10,4 +11,31 @@ class IMessage(Interface):
     title = schema.TextLine(
         title=_(u"Title"),
         required=True,
+    )
+
+    message = RichText(
+        title=_("Text"),
+        required=True,
+    )
+
+    start = schema.Datetime(
+        title=_(u"Start date"),
+        required=False,
+    )
+
+    end = schema.Datetime(
+        title=_(u"End date"),
+        required=False,
+    )
+
+    msg_type = schema.Choice(
+        title=_(u"Message type"),
+        required=True,
+        values=[_(u'Info'), _(u'Warning'), _(u'Important')]
+    )
+
+    location = schema.Choice(
+        title=_(u"Location"),
+        required=True,
+        values=[_(u'Full Site'), _(u'Homepage')]
     )
