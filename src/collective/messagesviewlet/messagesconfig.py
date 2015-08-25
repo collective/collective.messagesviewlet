fde2fde849eb3d0ca1d3658e4ce1d25c12db100c
Stephan Geulette <s.geulette@imio.be>
2015-08-25 09:51:11 +0200

    Overrided method to not catalog linked type

--- src/collective/messagesviewlet/messagesconfig.py
+++ src/collective/messagesviewlet/messagesconfig.py
@@ -4,4 +4,10 @@ from plone.dexterity.content import Container
 
 class MessagesConfig(Container):
     """
+        MessagesConfig class
     """
+
+    # This method is used by index methods.
+    # If None is returned, the linked content type is not catalogued
+    def _getCatalogTool(self):
+        return None
