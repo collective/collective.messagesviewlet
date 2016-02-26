b2e4075ee0cdd8238214c37de54013b859d77de9
Stephan Geulette <s.geulette@imio.be>
2016-02-26 08:45:00 +0100

    Pinned some versions

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'plone.api',
         'plone.app.dexterity',
         'plone.app.lockingbehavior',
-        'plone.formwidget.datetime',
+        'plone.formwidget.datetime >= 1.3',
         'setuptools',
     ],
     extras_require={
