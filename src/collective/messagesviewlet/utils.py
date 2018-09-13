7ff0fc86b2f04b1e96d5a77daf8735acab199655
Stephan Geulette <s.geulette@imio.be>
2018-09-13 16:00:55 +0200

    Adapted resource following plone version

--- src/collective/messagesviewlet/utils.py
+++ src/collective/messagesviewlet/utils.py
@@ -7,9 +7,9 @@ from zope.component import queryUtility
 from zope.i18n.interfaces import ITranslationDomain
 
 
-def _(msgid, context, domain='collective.messagesviewlet'):
+def _(msgid, context, domain='collective.messagesviewlet', mapping=None):
     translation_domain = queryUtility(ITranslationDomain, domain)
-    return translation_domain.translate(msgid, context=context.REQUEST)
+    return translation_domain.translate(msgid, context=context.REQUEST, mapping=mapping)
 
 
 def _richtextval(text):
