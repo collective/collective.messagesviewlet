378bc7358c1bf1fa2829210b3d6a717843667d7c
Christophe Boulanger <christophe.boulanger@imio.be>
2020-12-09 15:15:17 +0100

    Add new localmessageviewlet template and Update messagesviewlet with isLocalMessageAuthorized method to check if we can add a local message somewhere in a folder.

--- src/collective/messagesviewlet/browser/messagesviewlet.py
+++ src/collective/messagesviewlet/browser/messagesviewlet.py
@@ -2,13 +2,13 @@
 
 from collective.messagesviewlet import HAS_PLONE_5
 from collective.messagesviewlet.utils import get_messages_to_show
-from plone.app.layout.viewlets import ViewletBase
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import common
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 
 
-class MessagesViewlet(ViewletBase):
+class MessagesViewlet(common.ViewletBase):
     """This viewlet displays all messages from this product."""
-    render = ViewPageTemplateFile('./messagesviewlet.pt')
 
     def is_plone_5(self):
         return HAS_PLONE_5
@@ -19,3 +19,7 @@ class MessagesViewlet(ViewletBase):
     def getCSSClassName(self, msg_type):
         mapping_type = {'info': 'info', 'significant': 'warning', 'warning': 'error'}
         return 'portalMessage {0}'.format(mapping_type[msg_type])
+
+    def isLocalMessageAuthorized(self):
+        registry = getUtility(IRegistry)
+        return registry.get('messagesviewlet.authorize_local_message')
