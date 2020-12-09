fb31ffa423483650a34768b79b24b1385fad5898
Christophe Boulanger <christophe.boulanger@imio.be>
2020-12-09 15:16:52 +0100

    blacked file

--- src/collective/messagesviewlet/upgrades.py
+++ src/collective/messagesviewlet/upgrades.py
@@ -5,36 +5,45 @@ from plone.indexer.wrapper import IndexableObjectWrapper
 import logging
 
 
-logger = logging.getLogger('collective.messagesviewlet: upgrade. ')
+logger = logging.getLogger("collective.messagesviewlet: upgrade. ")
 
 
 def upgrade_to_1001(context):
     """ Avoid warning about unresolved dependencies """
-    setup = api.portal.get_tool('portal_setup')
+    setup = api.portal.get_tool("portal_setup")
     registry = setup.getImportStepRegistry()
-    config = {'collective-messagesviewlet-post-install': (u'browserlayer', u'controlpanel', u'cssregistry',
-                                                          u'propertiestool', u'rolemap', u'typeinfo', u'workflow'),
-              'collective-messagesviewlet-messages': ()}
+    config = {
+        "collective-messagesviewlet-post-install": (
+            u"browserlayer",
+            u"controlpanel",
+            u"cssregistry",
+            u"propertiestool",
+            u"rolemap",
+            u"typeinfo",
+            u"workflow",
+        ),
+        "collective-messagesviewlet-messages": (),
+    }
     for key, value in config.items():
         step = registry._registered.get(key)
         if step is not None:
-            step['dependencies'] = value
+            step["dependencies"] = value
     setup._p_changed = True
-    logger.info('Import step dependency corrected')
+    logger.info("Import step dependency corrected")
 
 
 def upgrade_to_2000(context):
     """
         Add timezone to start and end
     """
-    catalog = api.portal.get_tool('portal_catalog')
-    brains = catalog(portal_type='Message')
-    logger.info('Found {0} messages'.format(len(brains)))
+    catalog = api.portal.get_tool("portal_catalog")
+    brains = catalog(portal_type="Message")
+    logger.info("Found {0} messages".format(len(brains)))
     count = 0
     for brain in brains:
         obj = brain.getObject()
         correction = False
-        for attr in ('start', 'end'):
+        for attr in ("start", "end"):
             if getattr(obj, attr, False):
                 # use plone.indexer index to be sure we have same value
                 indexable_wrapper = IndexableObjectWrapper(obj, catalog)
@@ -45,4 +54,4 @@ def upgrade_to_2000(context):
         # reindex entire object to avoid datetime with/without TZ comparison
         # that breaks metadata update
         obj.reindexObject()
-    logger.info('Corrected {0} messages'.format(count))
+    logger.info("Corrected {0} messages".format(count))
