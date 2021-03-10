47275b790c57830d7a7bbb5c5943e7c8843c7e25
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-10 09:12:31 +0100

    Define a MessagesViewletBase and 2 others viewlets subclass.

--- src/collective/messagesviewlet/browser/messagesviewlet.py
+++ src/collective/messagesviewlet/browser/messagesviewlet.py
@@ -7,14 +7,17 @@ from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 
 
-class MessagesViewlet(common.ViewletBase):
+class MessagesViewletBase(common.ViewletBase):
     """This viewlet displays all messages from this product."""
+    location_filter = []
 
     def is_plone_5(self):
         return HAS_PLONE_5
 
     def getAllMessages(self):
-        return get_messages_to_show(self.context)
+        messages = get_messages_to_show(self.context)
+        messages = [m for m in messages if m.location in self.location_filter]
+        return messages
 
     def getCSSClassName(self, msg_type):
         mapping_type = {'info': 'info', 'significant': 'warning', 'warning': 'error'}
@@ -23,3 +26,15 @@ class MessagesViewlet(common.ViewletBase):
     def isLocalMessageAuthorized(self):
         registry = getUtility(IRegistry)
         return registry.get('messagesviewlet.authorize_local_message')
+
+
+class GlobalMessagesViewlet(MessagesViewletBase):
+    """Get global messages (homepage, messageconfig folder)"""
+    id = "messagesviewlet"
+    location_filter = ["fullsite", "homepage"]
+
+
+class LocalMessagesViewlet(MessagesViewletBase):
+    """Get local messages (From folderish)"""
+    id = "localmessagesviewlet"
+    location_filter = ["justhere", "fromhere"]
