2db67fcfa4878560fc0e5fa5eb2cf81a14b5da05
Christophe Boulanger <christophe.boulanger@imio.be>
2021-04-27 13:26:10 +0200

    Fix plone6 robot tests

--- src/collective/messagesviewlet/tests/test_robot.py
+++ src/collective/messagesviewlet/tests/test_robot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from collective.messagesviewlet import HAS_PLONE_5_AND_MORE
 from collective.messagesviewlet.testing import COLLECTIVE_MESSAGESVIEWLET_ACCEPTANCE_TESTING  # noqa
 from plone.app.testing import ROBOT_TEST_LEVEL
 from plone.testing import layered
@@ -8,14 +9,12 @@ import os
 import robotsuite
 import unittest
 
-IS_PLONE_5 = api.env.plone_version().startswith('5')
-
 
 def test_suite():
     suite = unittest.TestSuite()
     current_dir = os.path.abspath(os.path.dirname(__file__))
     robot_dir = os.path.join(current_dir, 'robot')
-    if IS_PLONE_5:
+    if HAS_PLONE_5_AND_MORE:
         robot_tests = [
             os.path.join('robot', doc) for doc in os.listdir(robot_dir)
             if doc.endswith('.p5.robot') and doc.startswith('test_')
