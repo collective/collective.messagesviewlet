06cea0102757b90f46027e3526c129414c6c3c3b
Christophe Boulanger <christophe.boulanger@imio.be>
2021-04-27 10:24:07 +0200

    Update less and js to support localMessagesViewlet

--- src/collective/messagesviewlet/browser/static/messagesviewlet.js
+++ src/collective/messagesviewlet/browser/static/messagesviewlet.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-  $("#messagesviewlet .close-button").click(function(){
+  function handler(event) {
     var m_uids = $.cookie('messagesviewlet');
     var m_uid = $(this).parent().attr('id');
     if (typeof m_uids === "undefined") {m_uids = m_uid;}
     else {m_uids += "|" + m_uid;}
     $.cookie('messagesviewlet', m_uids, { expires: 60 });
     $(this).parent().hide();
-    });
+    }
+  $("#messagesviewlet .close-button").bind("click",{}, handler);
+  $("#localmessagesviewlet .close-button").bind("click",{}, handler);
 });
