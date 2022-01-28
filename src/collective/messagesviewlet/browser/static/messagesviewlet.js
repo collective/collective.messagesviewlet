01bbe1c54f2ff26e4d0ef9d23192ab85cd055154
Christophe Boulanger <christophe.boulanger@imio.be>
2022-01-28 11:23:15 +0100

    Use js.cookie in plone5 profile instead of jquery.cookie. + Cookies.get instead of jquery syntax.
    Fix closing messages issue

--- src/collective/messagesviewlet/browser/static/messagesviewlet.js
+++ src/collective/messagesviewlet/browser/static/messagesviewlet.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+jQuery(document).ready(function ($) {
   function handler(event) {
-    var m_uids = $.cookie('messagesviewlet');
+    var m_uids = Cookies.get('messagesviewlet');
     var m_uid = $(this).parent().attr('id');
     if (typeof m_uids === "undefined") {m_uids = m_uid;}
     else {m_uids += "|" + m_uid;}
-    $.cookie('messagesviewlet', m_uids, { expires: 60 });
+    Cookies.set('messagesviewlet', m_uids, { expires: 60 });
     $(this).parent().hide();
     }
   $("#messagesviewlet .close-button").bind("click",{}, handler);
