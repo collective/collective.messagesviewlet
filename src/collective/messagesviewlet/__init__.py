1ea5f2bc0291959170f6eb72cdde5220316502d5
Rémi Dubois <remi.dubois@gmail.com>
2025-04-24 10:03:24 +0200

    feat: Python2 retrocompatibility

--- src/collective/messagesviewlet/__init__.py
+++ src/collective/messagesviewlet/__init__.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 
-from importlib.metadata import version, PackageNotFoundError
 from plone import api
 from zope.i18nmessageid import MessageFactory
+
+import six
+
+if six.PY2:
+    from pkg_resources import DistributionNotFound as PackageNotFoundError
+else:
+    from importlib.metadata import version, PackageNotFoundError
 
 
 _ = MessageFactory('collective.messagesviewlet')
@@ -11,7 +17,10 @@ _ = MessageFactory('collective.messagesviewlet')
 HAS_PLONE_5_AND_MORE = api.env.plone_version().startswith('5') or api.env.plone_version().startswith('6')
 
 try:
-    version('plone.app.event')
+    if six.PY2:
+        api.env.get_distribution('plone.app.event')
+    else:
+        version('plone.app.event')
     HAS_PAE = True
 except PackageNotFoundError:
     HAS_PAE = False
